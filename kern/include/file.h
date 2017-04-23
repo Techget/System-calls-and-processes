43abf807a0706101ec75f0f52864e689163081d7
eudaemonal <eudaemonal@gmail.com>
2017-04-23 11:36:45 +1000

    update file.c function retval

--- kern/include/file.h
+++ kern/include/file.h
@@ -15,12 +15,12 @@
  * Put your function declarations and data types here ...
  */
 
-int sys_open(const char *filename, int flags, mode_t mode);
-int sys_dup2(int oldfd, int newfd);
-int sys_close(int fd);
-int sys_read(int fd, void *buf, size_t count);
-int sys_write(int fd, const void *buf, size_t count);
-off_t sys_lseek(int fd, off_t offset, int whence);
+int sys_open(const char *filename, int flags, mode_t mode, int *retval);
+int sys_dup2(int oldfd, int newfd, int *retval);
+int sys_close(int fd, int *retval);
+int sys_read(int fd, void *buf, size_t count, int *retval);
+int sys_write(int fd, const void *buf, size_t count, int *retval);
+off_t sys_lseek(int fd, off_t offset, int whence, int *retval,int *retval1);
 
 
 
